26bc62d498b084dc3a94d27236f7aca7d1b07d78
Danylo Chelyuk <dchelyuk@gmail.com>
2024-03-27 18:11:52 -0700

    created FetchAccount.php Account info is displayed from database Added profile.png

--- PageFiles/CreateComment.php
+++ PageFiles/CreateComment.php
@@ -21,9 +21,11 @@ session_start();
  //$user=$_SESSION['username'] ;
  $body = $_POST['commentText'];
  //echo($user);
-//TODO: add sql call to get all data, save as variables, and pass to existing sql insert statement 
+ //TODO: add sql call to get all data, save as variables, and pass to existing sql insert statement
  //but it was easier for testing to have it there. update all data as it should be in the table. we need username to put beside comment
  // append it to end of comment using json
+
+//    $sql_username_call = "select username from Users where userid = 1";
 
 $sql = "INSERT INTO Comments (postId, userId, commentCreateDate, data) VALUES (8, 2,  cast(now() as datetime ), :body)";
 $stmt = $conn->prepare($sql);
