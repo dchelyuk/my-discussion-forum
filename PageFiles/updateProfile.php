924d66afaeeab6088f725f6a0df6a45b8c36d43b
Danylo Chelyuk <dchelyuk@gmail.com>
2024-04-11 22:45:18 -0700

    Fixed "update profile picture"

--- PageFiles/updateProfile.php
+++ PageFiles/updateProfile.php
@@ -8,35 +8,30 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $userId = $_SESSION['userId'];
 
-//$userId = 2;
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['userImage'])) {
 //    $username = trim($_POST['username']);
 //    $pass = password_hash(trim($_POST['password']), PASSWORD_DEFAULT);
     $image = $_FILES['userImage'];
 
-    $targetDirectory = "uploads/";
+    $targetDirectory = "../uploads/";
 
     $targetFile = $targetDirectory . basename($image['name']);
-    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-    print_r($targetFile);
+    $imageFileType = $image['type'];
 
-    if (!in_array($imageFileType, ['image/jpg', 'image/png', 'image/gif'])) {
+    if (!in_array($imageFileType, ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'])) {
         echo "Sorry, only JPG, PNG, & GIF files are allowed.";
     } elseif ($image['size'] > 10000000) {
         echo "Sorry, your file is too large.";
     } else {
-        $imageFile = $targetFile;
-        print_r($imageFile);
+        $imageFile = file_get_contents($image['name']);
         if (move_uploaded_file($image['tmp_name'], $targetFile)) {
-            echo " File moved";
 
             $stmt = $conn->prepare("INSERT INTO userImages (userId, contentType, image) VALUES (:userId,
             :contentType, :imageData) ON DUPLICATE KEY UPDATE userId = VALUES(userId), contentType =
                 VALUES(contentType), image = VALUES(image);");
             $stmt->bindParam(':userId', $userId);
-            $stmt->bindParam(':contentType', $contentType);
-            $stmt->bindParam(':imageData', $imageData, PDO::PARAM_LOB);
+            $stmt->bindParam(':contentType', $imageFileType);
+            $stmt->bindParam(':imageData', $imageFile, PDO::PARAM_LOB);
 
             try {
                 $stmt->execute();
@@ -48,6 +43,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['userImage'])) {
             echo "File upload failed";
         }
     }
+} else {
+    echo "Some error in file transfer";
+}
+
+if (file_exists($targetFile)) {
+    if (unlink($targetFile)) {
+        echo "File deleted successfully.";
+    } else {
+        echo "Error deleting file.";
+    }
+} else {
+    echo "File does not exist.";
 }
 
 $conn = null;
+
+header("Location: AccountPage.html");
+exit;
